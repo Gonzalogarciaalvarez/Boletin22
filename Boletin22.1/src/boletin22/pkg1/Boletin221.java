fd98f2b8cb0bafe22660daecc774ae750565f1d1
ggarciaalvarez <ggarciaalvarez@victoria26.danielcastelao.org>
2018-04-18 11:00:56 +0200

    Tiempo

---
+++ Boletin22.1/src/boletin22/pkg1/Boletin221.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin22.pkg1;
+
+/**
+ *
+ * @author ggarciaalvarez
+ */
+public class Boletin221 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Ventana vent = new Ventana();
+        vent.setVisible(true);
+        
+    }
+    
+}
